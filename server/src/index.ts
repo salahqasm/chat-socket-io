87709731f92bcc313c536574b72b4f1328cdb610
salahqasm <salahqasm@outlook.com>
2024-06-22 17:45:56 +0300

    init db

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import http from 'http';
 import { Server, Socket } from "socket.io";
 import { PORT } from './config';
 import { SocketHandlerModel } from './models/socketHandler';
+import { PrismaClient } from "@prisma/client"
+const pc = new PrismaClient();
 
 const app = express();
 app.use(express.json());
@@ -17,7 +19,11 @@ io.on("connection", (socket: Socket) => {
   SocketHandler.eventHandler(socket)
 })
 
-
+app.get("/test", async (req, res) => {
+  await pc.user.create({ data: { email: "a7a", password: "123", username: "123", fullName: "asd" } })
+  const data = await pc.user.findMany({ where: { email: "a7a" } });
+  res.json({ data })
+})
 //server health check endpoint
 app.get("/health", async (req: Request, res: Response) => {
   console.log("HealthCheck called")
